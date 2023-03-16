33bd3858648f7a12aa8f501de3e82b0478fa402f
alex <o.lototskyi@gmail.com>
2023-03-16 17:34:27 +0200

    moved components to filders, use arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 import './Expenses.css';
 
-function Expenses({items}) {
+const Expenses = ({items}) => {
     return (
         <Card className="expenses">
             <ExpenseItem 
